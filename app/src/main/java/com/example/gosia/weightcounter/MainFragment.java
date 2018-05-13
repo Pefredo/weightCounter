24ff426f8bfe2ca6088c4cef50c1c62144d8d422
GosiaZ <s11483@pjwstk.edu.pl>
2018-05-13 15:07:22 +0200

    main view

---
+++ app/src/main/java/com/example/gosia/weightcounter/MainFragment.java
@@ -0,0 +1,26 @@
+package com.example.gosia.weightcounter;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, viewGroup, false);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //createDiagram();
+
+    }
+}
